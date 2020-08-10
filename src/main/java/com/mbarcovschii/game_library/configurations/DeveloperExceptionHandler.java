ee988ad393d0d5e15d61b173615f9ff81fe62be6
mbarcovschii <barcvoschii.mihail@gmail.com>
2020-08-10 15:33:48 +0300

    Something

--- src/main/java/com/mbarcovschii/game_library/configurations/DeveloperExceptionHandler.java
+++ src/main/java/com/mbarcovschii/game_library/configurations/DeveloperExceptionHandler.java
@@ -6,13 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class DeveloperExceptionHandler {
+public class DeveloperExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DeveloperNotFoundException.class)
     public ResponseEntity<?> handleDeveloperNotFoundException(
